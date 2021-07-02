dce7bd03dca6605759f1227718170ba2f5630784
BrayanPr <basecoco1@gmail.com>
2021-07-02 10:34:23 -0600

    poniendolo wapo

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -20,88 +20,93 @@ const Background = {
 class Login extends React.Component {
   render() {
     return (
-    <ScrollView>
-      <View style={styles.container}>
-        <StatusBar backgroundColor="transparent" translucent={true} />
+      <ScrollView style={styles.container1}>
+        <View style={styles.container}>
+          <StatusBar backgroundColor="transparent" translucent={true} />
 
-       
 
-        <ImageBackground source={Background} style={styles.image}>
-          <View style={styles.layerColor}>
-            <Text style={styles.title}>Welcome</Text>
 
-            <View>
-              <View style={styles.logoContainer}>
-                <Image
-                  style={styles.logo}
-                  source={{
+          <ImageBackground source={Background} style={styles.image}>
+            <View style={styles.layerColor}>
+              <Text style={styles.title}>Welcome</Text>
+
+              <View>
+                <View style={styles.logoContainer}>
+                  <Image
+                    style={styles.logo}
+                    source={{
                       uri: 'https://image.flaticon.com/icons/png/512/3448/3448650.png',
                     }}></Image>
+                </View>
               </View>
-            </View>
 
-            <View style={styles.login}>
-              <View style={styles.inputContainer}>
-                <TextInput
-                  style={styles.form}
-                  placeholder="Student ID"
-                  placeholderTextColor={Colors.black}
+              <View style={styles.login}>
+                <View style={styles.inputContainer}>
+                  <TextInput
+                    style={styles.form}
+                    placeholder="Student ID"
+                    placeholderTextColor={Colors.black}
                   // onChangeText={text => { esq no sabemos si se usa unu
                   //     this.setState(prevState =>{
-                      //         let form = Object.assign({}, prevState.form);
-                      //         form.name = text;
-                      //         return {form};
-                      //     })
-                      // }}
-                      />
-                <TextInput
-                secureTextEntry={true}
-                style={styles.form}
-                placeholder="Password"
-                placeholderTextColor={Colors.black}
-                // onChangeText={text => {
-                    //     this.setState(prevState =>{
-                        //         let form = Object.assign({}, prevState.form);
-                        //         form.name = text;
-                        //         return {form};
-                        //     })
-                        // }}
-                        />
+                  //         let form = Object.assign({}, prevState.form);
+                  //         form.name = text;
+                  //         return {form};
+                  //     })
+                  // }}
+                  />
+                  <TextInput
+                    secureTextEntry={true}
+                    style={styles.form}
+                    placeholder="Password"
+                    placeholderTextColor={Colors.black}
+                  // onChangeText={text => {
+                  //     this.setState(prevState =>{
+                  //         let form = Object.assign({}, prevState.form);
+                  //         form.name = text;
+                  //         return {form};
+                  //     })
+                  // }}
+                  />
+                </View>
+
+                <TouchableOpacity
+                  style={styles.buttonDark}
+                  onPress={this.handlePress}>
+                  <Text style={styles.buttonDarkText}>LOGIN</Text>
+                </TouchableOpacity>
               </View>
 
-              <TouchableOpacity
-                style={styles.buttonDark}
-                onPress={this.handlePress}>
-                <Text style={styles.buttonDarkText}>LOGIN</Text>
-              </TouchableOpacity>
             </View>
-
+          </ImageBackground>
+          <View style={styles.signup}>
+            <Text style={styles.signupText}>Don't have an account?</Text>
+            <TouchableOpacity
+              style={styles.buttonLight}
+              onPress={this.handlePress}>
+              <Text style={styles.buttonLightText}>SIGN UP</Text>
+            </TouchableOpacity>
           </View>
-        </ImageBackground>
-            <View style={styles.signup}>
-              <Text style={styles.signupText}>Don't have an account?</Text>
-              <TouchableOpacity
-                style={styles.buttonLight}
-                onPress={this.handlePress}>
-                <Text style={styles.buttonLightText}>SIGN UP</Text>
-              </TouchableOpacity>
-            </View>
-      </View>
-    </ScrollView>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        
-        flexDirection: 'column',
+  container1: {
 
+    backgroundColor: Colors.white,
+
+  },
+  container: {
+    flex: 1,
+
+    flexDirection: 'column',
+    height: '100%',
     justifyContent: 'center',
 
     backgroundColor: Colors.white,
-  
+
   },
 
   image: {
@@ -110,9 +115,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderBottomLeftRadius: 200,
     borderBottomRightRadius: 200,
-    overflow: 'hidden', 
-    paddingBottom: '42%',
-  
+    overflow: 'hidden',
+    paddingBottom: '50%',
+
     // height: 100, // same width and height for the container
     // width: 200,
     // position: 'absolute', // position it in circle
@@ -121,8 +126,8 @@ const styles = StyleSheet.create({
   },
 
   logoContainer: {
-    alignSelf:'center',
-    
+    alignSelf: 'center',
+    marginTop: '-53%',
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -135,7 +140,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.white,
 
     width: 110,
-    height: 110,
+    height: 232,
     borderRadius: 90,
     position: 'absolute',
 
@@ -157,9 +162,9 @@ const styles = StyleSheet.create({
   },
 
   title: {
-    margin: '15%',
+    marginTop: '10%',
 
-    marginBottom: '30%',
+    marginBottom: '54%',
 
     fontSize: 80,
 
@@ -174,14 +179,14 @@ const styles = StyleSheet.create({
 
   login: {
     shadowColor: '#000',
-    
+
     shadowOffset: {
       width: 0,
       height: 5,
     },
 
-    marginTop: '-20%',
-    height: '80%',    
+    marginTop: '-40%',
+    height: '80%',
 
     shadowOpacity: 0.36,
 
@@ -208,7 +213,9 @@ const styles = StyleSheet.create({
 
   inputContainer: {
 
-    marginTop: '18%',
+    marginTop: '25%',
+
+    marginBottom: '-8%'
   },
 
   form: {
@@ -218,7 +225,7 @@ const styles = StyleSheet.create({
 
     borderBottomWidth: 1,
 
-    marginBottom: '6%',
+    marginBottom: '10%',
 
     width: 150,
 
@@ -228,11 +235,12 @@ const styles = StyleSheet.create({
   signup: {
     display: 'flex',
 
+    marginTop: '-10%',
+
     justifyContent: 'center',
 
     alignItems: 'center',
 
-    marginTop: '5%',
   },
 
   signupText: {
@@ -268,10 +276,10 @@ const styles = StyleSheet.create({
   },
   buttonDark: {
     width: '60%',
-    height: '0%',
-    marginTop: '75%',
+    height: '18%',
+    marginTop: '90%',
 
-    // marginBottom: 0,
+    marginBottom: '-90%',
 
     borderRadius: 15,
 
