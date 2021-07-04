9d670b4c7e99a6d826bc6282cbe8cf76b078b9d0
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-04 17:26:03 -0600

    layout set to show just title

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,20 +4,16 @@ import React from 'react'
 import {StyleSheet, Text} from 'react-native'
 
 class Layout extends React.Component{
+
     render(){
         return(
-            <Text style={styles.title}>Hola</Text>
+            <Text style={styles.title}>Nabar</Text>
         )
     }
 
 }
 
 const styles = StyleSheet.create({
-
-    title:{
-        fontFamily: "comfortaa"
-    }
-    
 })
 
 export default Layout
