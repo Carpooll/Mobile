6fd9d6d40c332d7b1584b4fb2705cf6d4d0accaf
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-04 19:28:34 -0600

    icons added but not working

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Stack from './src/components/Layout/Stack'
 
 // NAVBAR
 import { NavigationContainer } from '@react-navigation/native';
@@ -8,8 +7,11 @@ import {
   View,
   Text
 } from 'react-native'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+// import { Colors } from 'react-native/Libraries/NewAppScreen';
+import Colors from './src/res/Colors';
 
-function Notifications(){
+function Notifications() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Notifications!</Text>
@@ -17,8 +19,7 @@ function Notifications(){
   );
 }
 
-
-function Balance(){
+function Balance() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Balance</Text>
@@ -26,7 +27,7 @@ function Balance(){
   );
 }
 
-function Profile(){
+function Profile() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Profile</Text>
@@ -34,7 +35,7 @@ function Profile(){
   );
 }
 
-function HomeScreen(){
+function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
@@ -44,32 +45,48 @@ function HomeScreen(){
 
 const Tab = createBottomTabNavigator();
 
-
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Notifications" component={Notifications} />
-      <Tab.Screen name="Balance" component={Balance} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Home" component={HomeScreen} />
-    </Tab.Navigator>
-  );
-}
-
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Tab.Navigator
+        screenOptions={({ route }) => ({              // Default options to use for the screens in the navigator.
+          tabBarIcon: ({ focused, color, size }) => { //returns a React.Node, to display in the tab bar.
+            let iconName; // ????
+
+            if (route.name === 'Notifications') {
+              iconName = focused                  // focused == pressed
+                ? 'notifications'                 // if focused
+                : 'notifications-outline';        //else
+            } else if (route.name === 'Balance') {
+              iconName = focused
+                ? 'ios-list-box'
+                : 'ios-list';
+            } else if (route.name === 'Profile') {
+              iconName = focused
+                ? 'person-circle'
+                : 'person-circle-outline'
+            } else if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline'
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={Colors.blue} />;
+          },
+        })}
+
+        tabBarOptions={{
+          activeTintColor: Colors.blue,    // Color displayed when prop is clicked on
+          inactiveTintColor: Colors.black, // Color displayed
+        }}>
+
+        <Tab.Screen name="Notifications" component={Notifications} />
+        <Tab.Screen name="Balance" component={Balance} />
+        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-// const App = () => {
-//   return (
-//     <NavigationContainer>
-//       <Stack />
-//     </NavigationContainer>
-//   )
-// };
-
-// export default App;
