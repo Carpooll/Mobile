b5171b344b61f0410184b7f7331f64b240869094
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-07 19:05:07 -0600

    tabnavigator added, not tested

---
+++ src/components/AppNavigator/TabNavigator.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Image } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Stack from '../Layout/Stack'
+import Colors from '../../res/Colors'
+
+const Tabs = createBottomTabNavigator()
+
+const TabNavigator = () => {
+    return (
+        <Tabs.Navigator
+            tabBarOptions={{
+                showLabel: false,
+                tintColor: Colors.blueLight,
+                activeTintColor: Colors.blue,
+                style: {
+                    backgroundColor: Colors.white
+                }
+            }}>
+            {/* Each screen corresponds to an icon on the navbar */}
+            <Tab.Screen
+                name="Layout"
+                component={Stack}
+                options={{
+                    tabBarIcon: ({ size, color }) => (
+                        <Image
+                            style={{ tintColor: color, width: size, height: size }}
+                            source={{ uri: 'https://i.imgur.com/E8BiIPd.png' }}
+                        />
+                    )
+                }}
+            />
+        </Tabs.Navigator>
+    )
+}
+
+export default TabNavigator
