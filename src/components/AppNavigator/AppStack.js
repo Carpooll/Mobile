e4bf84f12d2786011be889474a4e81de02e5485a
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-07 18:26:35 -0600

    appstack added, not tested

---
+++ src/components/AppNavigator/AppStack.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack'
+import TabNavigator from "../Layout/TabNavigator.js"
+import Layout from "../Layout/Layout"
+import Colors from "../../res/Colors"
+
+const Stack = createStackNavigator()
+
+const AppStack = () => {
+    return (
+        <Stack.Navigator 
+            screenOptions={{
+                headerShown:false,
+                headerStyle: {
+                    backgroundColor: Colors.black,
+                    shadowColor: Colors.white
+                },
+                headerTintColor: Colors.white
+            }}
+        >
+            <Stack.Screen 
+                name="Landing" 
+                component={Layout}
+                options={{headerShown:false}}
+            />
+            {/* v Add here the rest of the screens once they are ready v */}
+            <Stack.Screen name="BadgesTabNavigator" component={TabNavigator}/>
+        </Stack.Navigator>
+    );
+}
+
+export default AppStack
