012b7459f8b3ddeb9e367a2498c0d80656c7431a
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-07-16 17:31:45 -0600

    all imports and screens needed added to appstack

--- src/components/AppNavigator/AppStack.js
+++ src/components/AppNavigator/AppStack.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import TabNavigator from "./TabNavigator.js"
 import Layout from "../Layout/Layout"
 import Colors from "../../res/Colors"
+
+import TabNavigator from "./TabNavigator.js"
+
+// Screens
+import Login from '../Login/Login'
+import HomeDriver from "../Home/HomeDriver"
+import HomePassenger1 from '../Home/HomePassenger'
+import SignupSelection from "../Signup/SignupSelection"     // Step 1 x
+import SignUpData from '../Signup/Signup'                   // Step 2 x
+import SignUpAddress from '../Signup/SignupAddress'         // Step 3 x
+import SignupPayment from '../Signup/SignupPayment'         // Step 4 (driver only) x
+import SignupCar from '../Signup/SignupCar'                 // Step 5 (driver only)
+import EditProfileDriver from '../Edit/EditProfileDriver'
+import EditProfilePassenger from "../Edit/EditProfilePassenger" 
+import DriverDetailsPrivate from '../Details/DetailsPrivate' 
+import ProfileDriver from "../Profiles/ProfileDriver" 
+import PassengerPublicProfile from '../Profiles/ProfilePassenger' 
+import PassengerPrivate from '../Profiles/ProfilePassengerPrivate'
+import Notifications from '../Notifications/Notifications' 
 
 const Stack = createStackNavigator()
 
@@ -18,6 +36,13 @@ const AppStack = () => {
                 headerTintColor: Colors.white
         }}>
 
+            <Stack.Screen name="Login" component={Login}/>
+            <Stack.Screen name="SignUpSelection" component={SignupSelection}/>
+            <Stack.Screen name="SignUpData" component={SignUpData}/>
+            <Stack.Screen name="SignUpAddress" component={SignUpAddress}/>
+            <Stack.Screen name="SignupPayment" component={SignupPayment}/>
+            <Stack.Screen name="SignupCar" component={SignupCar}/>
+            <Stack.Screen name="PassengerPublicProfile" component={PassengerPublicProfile}/>
             <Stack.Screen name="TabNavigator" component={TabNavigator}/>
 
         </Stack.Navigator>
