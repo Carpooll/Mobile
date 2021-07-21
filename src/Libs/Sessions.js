e0bdfe2e8535fc24ef4d7019ffbf05253237bcc9
unknown <a6519150007@utch.edu.mx>
2021-07-21 12:04:01 -0600

    creating storage and sessions files

---
+++ src/Libs/Sessions.js
@@ -0,0 +1,73 @@
+import Storage from './Storage';
+
+class UserSession {
+  static instance = new UserSession();
+
+  login = async body => {
+    try {
+      let request = await fetch(
+        `http://backend-heroku-link`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        }
+      );
+      let response = await request.json();
+      try {
+        let key = `token-${response.user.username}`;
+        await Storage.instance.store(key, response.token);
+        return response.user.username;
+      } catch (err) {
+        return response;
+      }
+    } catch (err) {
+      console.log('Login error', err);
+      throw Error(err);
+    }
+  };
+
+  logout = async key => {
+    try {
+      await Storage.instances.remove(key);
+      return true;
+    } catch (err) {
+      console.log('logout err', err);
+      return false;
+    }
+  };
+
+  signup = async body => {
+    try {
+      let request = await fetch(`http://127.0.0.1:8000/users/signup/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      let response = await request.json();
+      if (typeof response.username == 'string') {
+        return response.username;
+      } else {
+        return response;
+      }
+    } catch (err) {
+      console.log('signup err', err);
+      throw Error(err);
+    }
+  };
+
+  getToken = async key => {
+    try {
+      return await Storage.instance.get(key);
+    } catch (err) {
+      console.log('Get token error', err);
+    }
+  };
+}
+
+export default UserSession;
