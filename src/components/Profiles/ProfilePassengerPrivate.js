fb1e7d6cfc4d823ebeecc95035d0c5a26b08b499
unknown <a6519150007@utch.edu.mx>
2021-07-22 12:52:07 -0600

    Adding the map view

--- src/components/Profiles/ProfilePassengerPrivate.js
+++ src/components/Profiles/ProfilePassengerPrivate.js
@@ -1,211 +1,225 @@
-import React from 'react'
+import React from 'react';
 import {
-    Text,
-    View,
-    StatusBar,
-    ImageBackground,
-    TouchableOpacity,
-    StyleSheet,
-    Image,
-    ScrollView,
-    Dimensions,
+  Text,
+  View,
+  StatusBar,
+  ImageBackground,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView,
+  Dimensions,
 } from 'react-native';
-import Fonts from '../../res/Fonts'
+import Fonts from '../../res/Fonts';
 import Colors from '../../res/Colors';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 // NEEDS TO CHANGE TO DYNAMIC DATA
 
 class PassengerPrivate extends React.Component {
+  state = {
+    markers: {
+      latitude: 28.6491049,
+      longitude: -106.0282516,
+      longitudeDelta: 0.0,
+      latitudeDelta: 0.001,
+    },
+  };
 
-    handlePress = () => {
-        this.props.navigation.navigate('EditProfilePassenger')
-    }
+  handlePress = () => {
+    this.props.navigation.navigate('EditProfilePassenger');
+  };
 
-    render() {
-        return (
-            <ScrollView style={Styles.Container}>
-                <StatusBar backgroundColor="transparent" translucent={true} />
-                <View style={Styles.imageContainer}>
-                    <Image
-                        style={Styles.image}
-                        source={{
-                            uri: 'https://images.unsplash.com/photo-1624759314986-43bee161a691?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-                        }} />
-                </View>
-                <View style={Styles.infoContainer}>
-                    <Text style={Styles.userName}>Brayan Prieto</Text>
-                    <Text style={Styles.schoolId}>35416654231</Text>
-                    <Text style={Styles.phone}>614-522-88-99</Text>
+  render() {
+    return (
+      <ScrollView style={Styles.Container}>
+        <StatusBar backgroundColor="transparent" translucent={true} />
+        <View style={Styles.imageContainer}>
+          <Image
+            style={Styles.image}
+            source={{
+              uri: 'https://images.unsplash.com/photo-1624759314986-43bee161a691?ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDMzfHRvd0paRnNrcEdnfHxlbnwwfHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
+            }}
+          />
+        </View>
+        <View style={Styles.infoContainer}>
+          <Text style={Styles.userName}>Brayan Prieto</Text>
+          <Text style={Styles.schoolId}>35416654231</Text>
+          <Text style={Styles.phone}>614-522-88-99</Text>
 
-                    <Text style={Styles.userTitle}>Your current balance</Text>
-                    <View style={Styles.profitContainer}>
-                        <Text style={Styles.userInfo}>$ 350.00</Text>
-                    </View>
+          <Text style={Styles.userTitle}>Your current balance</Text>
+          <View style={Styles.profitContainer}>
+            <Text style={Styles.userInfo}>$ 350.00</Text>
+          </View>
 
-                    <Text style={Styles.loc}>Location</Text>
-                    <View style={Styles.mapContainer}>
-                        <Image
-                            style={Styles.mapContainer}
-                            source={{
-                                uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia.wired.com%2Fphotos%2F5a6a61938c669c70314b300d%2F191%3A100%2Fpass%2FGoogle-Map-US_10.jpg&f=1&nofb=1',
-                            }} />
-                    </View>
-                </View>
+          <Text style={Styles.loc}>Location</Text>
+          <View style={Styles.containerMap}>
+            <MapView
+              provider={PROVIDER_GOOGLE}
+              style={Styles.map}
+              initialRegion={this.state.markers}
+              >
+                  <Marker coordinate={this.state.markers} />
+              </MapView>
+          </View>
+        </View>
 
-                <TouchableOpacity style={Styles.darkButton} onPress={this.handlePress}>
-                    <Text style={Styles.darkButtonText}>EDIT</Text>
-                </TouchableOpacity>
-
-            </ScrollView>
-        )
-    }
+        <TouchableOpacity style={Styles.darkButton} onPress={this.handlePress}>
+          <Text style={Styles.darkButtonText}>EDIT</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    );
+  }
 }
 
 var height = Dimensions.get('window').height;
-var width = Dimensions.get('window').width
-var iconSize = height * .15
-var borderTop = height * .10
-var FormWidth = width * .69
-var FormHeight = height * .7
+var width = Dimensions.get('window').width;
+var iconSize = height * 0.15;
+var borderTop = height * 0.1;
+var FormWidth = width * 0.69;
+var FormHeight = height * 0.7;
 
 const Styles = StyleSheet.create({
-    Container: {
-        backgroundColor: Colors.blue,
-        position: 'relative',
-        zIndex: 0
+  Container: {
+    backgroundColor: Colors.blue,
+    position: 'relative',
+    zIndex: 0,
+  },
+  infoContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 135,
+    height: height * 0.7,
+    width: FormWidth,
+    alignSelf: 'center',
+    padding: 'auto',
+
+    backgroundColor: Colors.white,
+    borderRadius: 15,
+    position: 'relative',
+  },
+  image: {
+    alignSelf: 'center',
+    height: iconSize,
+    width: iconSize,
+    borderRadius: iconSize / 2,
+  },
+  imageContainer: {
+    marginTop: borderTop,
+    alignSelf: 'center',
+    height: iconSize,
+    width: iconSize,
+    backgroundColor: Colors.white,
+    position: 'absolute',
+    borderRadius: iconSize / 2,
+    zIndex: 2,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
     },
-    infoContainer: {
+    shadowOpacity: 0.51,
+    shadowRadius: 13.16,
 
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 135,
-        height: height * .7,
-        width: FormWidth,
-        alignSelf: 'center',
-        padding: 'auto',
+    elevation: 20,
+  },
+  containerMap: {
+    ...StyleSheet.absoluteFillObject,
+    height: "25%",
+    width: 200,
+    marginTop:350,
+    marginLeft: 35,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 
-        backgroundColor: Colors.white,
-        borderRadius: 15,
-        position: 'relative',
+  userName: {
+    display: 'flex',
+    marginTop: -140,
+    color: Colors.black,
+    fontSize: 20,
+  },
+  schoolId: {
+    marginTop: 10,
+    color: Colors.black,
+    fontSize: Fonts.miniButtons,
+  },
+  phone: {
+    marginTop: 10,
+    color: Colors.black,
+    fontSize: Fonts.miniButtons,
+  },
 
-    },
-    image: {
-        alignSelf: 'center',
-        height: iconSize,
-        width: iconSize,
-        borderRadius: iconSize / 2,
+  userTitle: {
+    color: Colors.blue,
+    fontSize: 20,
 
-    },
-    imageContainer: {
-        marginTop: borderTop,
-        alignSelf: 'center',
-        height: iconSize,
-        width: iconSize,
-        backgroundColor: Colors.white,
-        position: 'absolute',
-        borderRadius: iconSize / 2,
-        zIndex: 2,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 10,
-        },
-        shadowOpacity: 0.51,
-        shadowRadius: 13.16,
+    marginTop: 20,
+  },
 
-        elevation: 20,
-    },
+  profitContainer: {
+    marginTop: '5%',
+    marginBottom: '2%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
 
-    userName: {
-        display: 'flex',
-        marginTop: 0,
-        color: Colors.black,
-        fontSize: 20,
-    },
-    schoolId: {
-        marginTop: 10,
-        color: Colors.black,
-        fontSize: Fonts.miniButtons,
-    },
-    phone: {
-        marginTop: 10,
-        color: Colors.black,
-        fontSize: Fonts.miniButtons,
-    },
+    width: 94,
+    height: 44,
 
-    userTitle: {
-        color: Colors.blue,
-        fontSize: 20,
+    backgroundColor: Colors.white,
 
-        marginTop: 20,
-    },
+    borderRadius: 7,
+    zIndex: 2,
 
-    profitContainer: {
-        marginTop: '5%',
-        marginBottom: '2%',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+    shadowColor: Colors.black,
+    shadowOpacity: 0.8,
+    elevation: 6,
+    shadowRadius: 15,
+    shadowOffset: {width: -5, height: -30},
+  },
 
-        width: 94,
-        height: 44,
+  profitTime: {
+    fontSize: 10,
+    color: '#A4A4A4',
+  },
 
-        backgroundColor: Colors.white,
+  mapContainer: {
+    height: 90,
+    width: 100,
+    borderRadius: 7,
+    zIndex: 2,
 
+    backgroundColor: Colors.blue,
+  },
 
-        borderRadius: 7,
-        zIndex: 2,
+  darkButton: {
+    alignSelf: 'center',
+    height: FormHeight * 0.1,
+    marginTop: -25, //590
+    width: FormWidth * 0.6,
+    borderRadius: 15,
 
-        shadowColor: Colors.black,
-        shadowOpacity: 0.8,
-        elevation: 6,
-        shadowRadius: 15,
-        shadowOffset: { width: -5, height: -30 },
-    },
+    fontSize: Fonts.miniButtons,
+    backgroundColor: Colors.black,
 
-    profitTime: {
-        fontSize: 10,
-        color: '#A4A4A4',
-    },
+    justifyContent: 'center',
 
-    mapContainer: {
-        height: 90,
-        width: 100,
-        borderRadius: 7,
-        zIndex: 2,
+    zIndex: 5,
+  },
 
-        backgroundColor: Colors.blue
-    },
+  darkButtonText: {
+    alignSelf: 'center',
+    color: Colors.white,
+  },
+  loc: {
+    color: Colors.blue,
+    fontSize: 20,
+    marginTop: 20,
+    marginBottom: '3%',
+  },
+});
 
-    darkButton: {
-
-        alignSelf: 'center',
-        height: FormHeight * .1,
-        marginTop: -25, //590
-        width: FormWidth * .6,
-        borderRadius: 15,
-
-        fontSize: Fonts.miniButtons,
-        backgroundColor: Colors.black,
-
-        justifyContent: 'center',
-
-        zIndex: 5,
-
-    },
-
-    darkButtonText: {
-
-        alignSelf: 'center',
-        color: Colors.white
-    },
-    loc: {
-        color: Colors.blue,
-        fontSize: 20,
-        marginTop: 20,
-        marginBottom: '3%'
-    }
-})
-
-export default PassengerPrivate
+export default PassengerPrivate;
