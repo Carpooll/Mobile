bb8adc3b0ed2644ef85d45e6902e3b418f10edfd
unknown <a6519150007@utch.edu.mx>
2021-08-05 18:28:44 -0600

    fixing api key problems

--- src/components/LatLong/LatLong.js
+++ src/components/LatLong/LatLong.js
@@ -1,11 +1,12 @@
+//Getting lat and long with the address that the user puts by hand.
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Geocoder from 'react-native-geocoding';
 import Geolocation from 'react-native-geolocation-service';
-import config from '../../../config'
+import api from '../../../config'
 
-Geocoder.init(config.API_KEY);
-Geocoder.init(config.API_KEY, {language: 'es'});
+Geocoder.init(api);
+Geocoder.init(api, {language: 'es'});
 
 export default class Location extends Component {
   constructor() {
