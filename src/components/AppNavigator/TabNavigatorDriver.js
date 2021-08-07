5e1aed7d5b7a4af950852df4a5e2af584d9d345e
unknown <a6519150007@utch.edu.mx>
2021-08-07 14:10:18 -0600

    selecting between home with passengers and whithout them

--- src/components/AppNavigator/TabNavigatorDriver.js
+++ src/components/AppNavigator/TabNavigatorDriver.js
@@ -1,118 +1,142 @@
-import React from 'react'
-import { Image } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import Stack from '../Layout/Stack'
-import Colors from '../../res/Colors'
-import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Stack from '../Layout/Stack';
+import Colors from '../../res/Colors';
+import {createStackNavigator} from '@react-navigation/stack';
 
 // SCREENS
-import Notifications from '../Notifications/Notifications'
+import Notifications from '../Notifications/Notifications';
 // Profiles
-import ProfileDriver from '../Profiles/ProfileDriver'
-import PassengerPublicProfile from '../Profiles/ProfilePassenger'
+import ProfileDriver from '../Profiles/ProfileDriver';
+import PassengerPublicProfile from '../Profiles/ProfilePassenger';
 // Home
-//import HomeDriver from '../Home/HomeDriver'
-import screens from '../DriverHomeMaps/DriverHomeMap'
+import HomeDriver from '../Home/HomeDriver';
+import screens from '../DriverHomeMaps/DriverHomeMap';
 // Edit
-import EditProfileDriver from '../Edit/EditProfileDriver'
+import EditProfileDriver from '../Edit/EditProfileDriver';
 
+import * as vars from '../DriverHomeMaps/DriverHomeMap';
+console.log('tabnavifator', vars.noPassengers);
 
-const Tabs = createBottomTabNavigator()
+const Tabs = createBottomTabNavigator();
 
 // Stack to redirect to screens between tab navigator (HOME)
 const HomeStack = createStackNavigator();
 function HomeStackScreen() {
-    return (
-        <HomeStack.Navigator
-            tabBarOptions={{
-                // showLabel: false,
-                tintColor: Colors.blueLight,
-                activeTintColor: Colors.blue,
-                style: {
-                    backgroundColor: Colors.white
-                }
-            }}
-        >
-            <HomeStack.Screen name="HomeDriver" component={screens} options={{ headerShown: false }} />
-            <HomeStack.Screen name="PassengerPublicProfile" component={PassengerPublicProfile} options={{ headerShown: false }} />
-        </HomeStack.Navigator>
-    )
+  return (
+    <HomeStack.Navigator
+      tabBarOptions={{
+        // showLabel: false,
+        tintColor: Colors.blueLight,
+        activeTintColor: Colors.blue,
+        style: {
+          backgroundColor: Colors.white,
+        },
+      }}>
+          
+      {vars.noPassengers ? (
+       
+        <HomeStack.Screen
+          name="HomeDriverNoPassengers"
+          component={HomeDriver}
+          options={{headerShown: false}}
+        />
+      ) : (
+        <HomeStack.Screen
+          name="HomeDriver"
+          component={screens}
+          options={{headerShown: false}}
+        />
+      )}
+      <HomeStack.Screen
+        name="PassengerPublicProfile"
+        component={PassengerPublicProfile}
+        options={{headerShown: false}}
+      />
+    </HomeStack.Navigator>
+  );
 }
 
 // Stack to redirect to screens between tab navigator (PROFILE)
-const ProfileStack = createStackNavigator()
+const ProfileStack = createStackNavigator();
 function ProfileStackScreen() {
-    return (
-        <ProfileStack.Navigator
-            tabBarOptions={{
-                // showLabel: false,
-                headerShown: false,
-                tintColor: Colors.blueLight,
-                activeTintColor: Colors.blue,
-                style: {
-                    backgroundColor: Colors.white
-                }
-            }}
-        >
-            <ProfileStack.Screen name="ProfileDriver" component={ProfileDriver} options={{ headerShown: false }} />
-            <HomeStack.Screen name="EditProfileDriver" component={EditProfileDriver} options={{ headerShown: false }} />
-        </ProfileStack.Navigator>
-    )
+  return (
+    <ProfileStack.Navigator
+      tabBarOptions={{
+        // showLabel: false,
+        headerShown: false,
+        tintColor: Colors.blueLight,
+        activeTintColor: Colors.blue,
+        style: {
+          backgroundColor: Colors.white,
+        },
+      }}>
+      <ProfileStack.Screen
+        name="ProfileDriver"
+        component={ProfileDriver}
+        options={{headerShown: false}}
+      />
+      <HomeStack.Screen
+        name="EditProfileDriver"
+        component={EditProfileDriver}
+        options={{headerShown: false}}
+      />
+    </ProfileStack.Navigator>
+  );
 }
-
 
 const TabNavigator = () => {
-    return (
-        <Tabs.Navigator
-            tabBarOptions={{
-                // showLabel: false,
-                headerShown: false,
-                tintColor: Colors.blueLight,
-                activeTintColor: Colors.blue,
-                style: {
-                    backgroundColor: Colors.white
-                }
-            }}>
+  return (
+    <Tabs.Navigator
+      tabBarOptions={{
+        // showLabel: false,
+        headerShown: false,
+        tintColor: Colors.blueLight,
+        activeTintColor: Colors.blue,
+        style: {
+          backgroundColor: Colors.white,
+        },
+      }}>
+      {/* Each screen corresponds to an icon on the navbar */}
+      <Tabs.Screen
+        name="Notifications"
+        component={Notifications}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Image
+              style={{tintColor: color, width: size, height: size}}
+              source={{uri: 'https://i.imgur.com/c4ndYby.png'}}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        component={ProfileStackScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Image
+              style={{tintColor: color, width: size, height: size}}
+              source={{uri: 'https://i.imgur.com/lAdvcUO.png'}}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Home"
+        component={HomeStackScreen}
+        options={{
+          tabBarIcon: ({size, color}) => (
+            <Image
+              style={{tintColor: color, width: size, height: size}}
+              source={{uri: 'https://i.imgur.com/E8BiIPd.png'}}
+            />
+          ),
+        }}
+      />
+    </Tabs.Navigator>
+  );
+};
 
-            {/* Each screen corresponds to an icon on the navbar */}
-            <Tabs.Screen
-                name="Notifications"
-                component={Notifications}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Image
-                            style={{ tintColor: color, width: size, height: size }}
-                            source={{ uri: 'https://i.imgur.com/c4ndYby.png' }}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="Profile"
-                component={ProfileStackScreen}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Image
-                            style={{ tintColor: color, width: size, height: size }}
-                            source={{ uri: 'https://i.imgur.com/lAdvcUO.png' }}
-                        />
-                    )
-                }}
-            />
-            <Tabs.Screen
-                name="Home"
-                component={HomeStackScreen}
-                options={{
-                    tabBarIcon: ({ size, color }) => (
-                        <Image
-                            style={{ tintColor: color, width: size, height: size }}
-                            source={{ uri: 'https://i.imgur.com/E8BiIPd.png' }}
-                        />
-                    )
-                }}
-            />
-        </Tabs.Navigator>
-    )
-}
-
-export default TabNavigator
+export default TabNavigator;
